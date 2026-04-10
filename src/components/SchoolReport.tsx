94cc069223df69e1308e741c7f095de547f7b07d
Kevin Shah <kevinlshah@gmail.com>
2026-04-10 15:53:52 -0700

    Remove EarlyScouts Deep Dive badge from hero section

--- src/components/SchoolReport.tsx
+++ src/components/SchoolReport.tsx
@@ -914,13 +914,7 @@ export default function SchoolReport({
         )}
         <div className={styles.heroColorWash} />
         <div className={styles.heroOverlay}>
-          <div className={styles.heroBadge}>
-            <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/>
-              <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/>
-            </svg>
-            EarlyScouts Deep Dive
-          </div>
+
           <h1 className={[
             styles.heroTitle,
             school.name.length > 50 ? styles.heroTitleLong : school.name.length >= 30 ? styles.heroTitleMed : ''
